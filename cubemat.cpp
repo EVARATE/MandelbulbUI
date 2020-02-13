d5ecf3e56ee9700248d7b483e5be10daa0f327f9
Mo <GreeeNLambda@googlemail.com>
2020-02-13 21:29:13 +0100

    Renaming to MandelbulbUI

--- cubemat.cpp
+++ cubemat.cpp
@@ -1,20 +1,20 @@
 /*
 Copyright 2019, 2020 Sebastian Motzet
 
-This file is part of MandelbulbUI_V2.
+This file is part of MandelbulbUI.
 
-MandelbulbUI_V2 is free software: you can redistribute it and/or modify
+MandelbulbUI is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-MandelbulbUI_V2 is distributed in the hope that it will be useful,
+MandelbulbUI is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with MandelbulbUI_V2.  If not, see <https://www.gnu.org/licenses/>.
+along with MandelbulbUI.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "cubemat.h"
@@ -143,7 +143,7 @@ void cubeMat::saveObj(std::string fileName){
     std::string polyBuffer;
     std::string faceBuffer;
     int vCounter = 0;
-    ofile << "#DANGER: File generated via MandelbulbUI_V2:\n";
+    ofile << "#DANGER: File generated via MandelbulbUI:\n";
     ofile << "o Mandelbulb\n";
     for(int i = 0; i < cubeSlots.size(); ++i){
     for(int j = 0; j < cubeSlots[i].size(); ++j){
